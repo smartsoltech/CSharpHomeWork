c0cbf59e3d6d79b4645ca501fe82850255cdc898
Andrew Choi <a.choi@smartsoltech.kr>
2022-08-29 23:54:06 +0900

    Task 4 added

---
+++ Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter a digit> ");
+int value = int.Parse(Console.ReadLine() ?? "0");
+int i = 0;
+for (i=1; i<=value; i++)
+{
+    if (i%2==0) 
+    {
+       Console.Write(" ");
+       Console.Write(i);
+
+    }
+    
+}
