5d919f0b7f4ccc3acfcbd6f0adcb3218261d9f3b
Andrew Choi <a.choi@smartsoltech.kr>
2022-08-29 23:37:19 +0900

    Second Home Task Find min and max values of 3 console-entered digits

---
+++ Task2_Max_min_of_3_digits/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Enter 1st number> ");
+int numA = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Enter 2nd digit>>");
+int numB = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Enter 3rd digit>>");
+int numC = int.Parse(Console.ReadLine() ?? "0");
+
+int min = 0;
+int max = 0;
+if (numA < numB )
+{
+    min = numA;
+    max = numB;
+}
+else
+{
+    min = numB;
+    max = numA;
+}
+if (numC>max)
+{
+    min = min;
+    max = numC;
+}
+else 
+{
+    min = numC;
+    max = max;
+}
+    Console.Write("Min -> ");
+    Console.WriteLine(min);
+    Console.Write("Max -> ");
+    Console.WriteLine(max);
+
