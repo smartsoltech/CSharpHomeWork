909ee22a2f532d1a493aef0a8d74f38eaf2b53d8
Andrew Choi <a.choi@smartsoltech.kr>
2022-08-29 23:23:07 +0900

    Init commit

---
+++ Task1_max_of_two_digits/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Enter 1st number> ");
+int numA = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Enter 2nd digit>>");
+int numB = int.Parse(Console.ReadLine() ?? "0");
+
+if (numA < numB )
+{
+    Console.Write("Min -> ");
+    Console.WriteLine(numA);
+    Console.Write("Max -> ");
+    Console.WriteLine(numB);
+
+}
+else 
+{
+    Console.Write("Min -> ");
+    Console.WriteLine(numB);
+    Console.Write("Max -> ");
+    Console.WriteLine(numA);
+}
+
