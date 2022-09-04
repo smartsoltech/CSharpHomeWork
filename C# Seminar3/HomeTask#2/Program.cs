4c26aebec4e0c7fd9e12a47cb099464ac4296a13
Andrew Choi <a.choi@smartsoltech.kr>
2022-09-04 16:23:40 +0900

    Task#2 solved. Added.

---
+++ C# Seminar3/HomeTask#2/Program.cs
@@ -0,0 +1,5 @@
+﻿int value = new Random().Next(99, 999);
+System.Console.WriteLine(value);
+int f_tmp_value = value / 100;
+int s_tmp_value = value / 100;
+System.Console.WriteLine($"Первая Цифра - {value/100}, Последняя цифра - {value%10}, Сгенерированое трех-значное число без средней цифры - {value/100}{value%10}");
