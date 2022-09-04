157aec0a2e36551d438e0ed76f96f52b085d00ac
Andrew Choi <a.choi@smartsoltech.kr>
2022-09-04 16:08:38 +0900

    Task #3. Seminar 3.

--- C# Seminar3/HomeTask#3/Program.cs
+++ C# Seminar3/HomeTask#3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите 2-3-х значное число> ");
+int value = int.Parse(Console.ReadLine() ?? "0");
+int tmp_value = value % 10;
+if ((value / 100) > 10)
+{
+    System.Console.WriteLine($"Введеное число: {value} имеет 4 цифры. Не буду считать!");
+}
+else if (((value / 10) == 0) || ((value / 10) < 10))
+{
+    System.Console.WriteLine($"Введеное число: {value}, Двузначное. Третьей цифры нет");
+}
+else if (((value/100) > 0) || ((value / 100) < 10))
+{
+    System.Console.WriteLine($"Введеное число: {value} Трех-значное, Последняя цифра - {tmp_value}, Первая цифра - {value / 100}");
+}
