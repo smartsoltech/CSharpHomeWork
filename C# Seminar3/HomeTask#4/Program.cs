02d3b589644cd695eef8a5e14d848be980614d7a
Andrew Choi <a.choi@smartsoltech.kr>
2022-09-04 16:35:40 +0900

    added Task 1, TAsk2 Fully solved all tasks

--- C# Seminar3/HomeTask#4/Program.cs
+++ C# Seminar3/HomeTask#4/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите Трех-значное число > ");
+string promt = System.Console.ReadLine();
+int value = int.Parse(promt);
+System.Console.WriteLine($"   Вы ввели: {value}");
+int tmp_value = value / 10;
+System.Console.WriteLine($" Вторая цифра - {tmp_value%10},");
