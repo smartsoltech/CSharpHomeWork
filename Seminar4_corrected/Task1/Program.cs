49c90e6e63d658aced55e79a14e320a7b9e8a85c
Andrew Choi <a.choi@smartsoltech.kr>
2022-09-11 19:57:39 +0900

    Task1 Solved

--- Seminar4_corrected/Task1/Program.cs
+++ Seminar4_corrected/Task1/Program.cs
@@ -8,29 +8,24 @@ int Prompt(string message)
     return Convert.ToInt32(Console.ReadLine());
 }
 
+
+double Pow(int digit, int pow)
+{
+    double result = 1;
+    for (int i = 0; i < pow; i++)
+    {
+        result *= digit;
+
+    }
+return result;
+}
+
+void Display(int digit, int pow, double number)
+{
+    System.Console.WriteLine($"{digit} в степени {pow}  =  {number}");
+}
+
 int digit = Prompt("Введите число > ");
 int pow = Prompt("Введите степень > ");
 
-void Display(int digit, int pow)
-{
-    long itog = 0;
-    double res = 0;
-    for (int i = 0; i <= pow; i++)
-    {
-        itog = Convert.ToUInt32(Math.Pow(digit, pow));
-        res *= digit;
-    }
-    System.Console.WriteLine($"Число {digit} в степени {pow} = {itog}, tmp_itog = {res}");
-
-    /*              xPow = x;
-                for (int o = 0; o < n; o++)
-                {
-                    xPow *= x;
-                }
-                step *= (-t * x) / ((2*i)*(2 * i + 1));
-                S += step;
-                
-    */
-
-}
-Display(digit, pow);
+Display(digit, pow, Pow(digit, pow));
