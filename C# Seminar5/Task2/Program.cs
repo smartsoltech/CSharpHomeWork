533ba932afd94346ac0b2f11693a07f5b086f3ea
Andrew Choi <a.choi@smartsoltech.kr>
2022-09-15 00:21:59 +0900

    Seminar5/Task2 Fill array with random numbers and count a quantity of elements, placed on position, that numberr coulde divisioned by 2

---
+++ C# Seminar5/Task2/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+*/
+int Prompt(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int[] GenerateArray(int size)
+{
+    int[] numbers = new int[size];
+    System.Console.Write("[");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100, 1000);
+        System.Console.Write($"{numbers[i]} ");
+    }
+    System.Console.WriteLine("]");
+    System.Console.WriteLine();
+    return numbers;
+}
+
+int Main(int[] array)
+{
+    int[] chetnoe = new int[array.Length];
+    int count = 0;
+    for (int i = 0; i <= array.Length-1; i=i+2)
+    {
+            chetnoe[i] = array[i];
+            count++;
+            System.Console.WriteLine($"{chetnoe[i]} - Нечетная позиция!");
+    }
+    return count;
+
+}
+
+int ArrayLength = Prompt("Введите длину массива > ");
+int[] array = GenerateArray(ArrayLength);
+int result = Main(array);
+
+System.Console.WriteLine($" Количество элементов на нечетной позиции  - {result}");
