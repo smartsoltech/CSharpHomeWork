58a3a015bf580b962e525a4933ec36f7414d07b3
Andrew Choi <a.choi@smartsoltech.kr>
2022-09-15 00:13:31 +0900

    Seminar 5 Task#1 fill array with random 3-lettered integer digits and count quantity of elements could be divisioned by 2

---
+++ C# Seminar5/Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+*/
+
+int Prompt(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int[] GenerateArray(int size)
+{
+    int[] numbers = new int[size];
+    System.Console.Write("[");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100, 1000);
+        System.Console.Write($"{numbers[i]} ");
+    }
+    System.Console.WriteLine("]");
+    System.Console.WriteLine();
+    return numbers;
+}
+
+int Main(int[] array)
+{
+    int[] chetnoe = new int[array.Length];
+    int count = 0;
+    for (int i = 0; i <= array.Length - 1; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            chetnoe[i] = array[i];
+            count++;
+            System.Console.WriteLine($"{chetnoe[i]} - Четное число!");
+        }
+    }
+    return count;
+
+}
+
+int ArrayLength = Prompt("Введите длину массива > ");
+int[] array = GenerateArray(ArrayLength);
+int result = Main(array);
+
+System.Console.WriteLine($" Количество четных элементов - {result}");
--- Seminar4_corrected/Task2/Program.cs
+++ Seminar4_corrected/Task2/Program.cs
@@ -2,3 +2,22 @@
 452 -> 11
 82 -> 10
 9012 -> 12*/
+
+int Prompt(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int input = Prompt("Введите число > ");
+
+int tmp_len = input;
+int sum = 0;
+while (input < 0)
+{
+    tmp_len = input / 10;
+    sum += tmp_len;
+    //return sum;
+    System.Console.WriteLine(sum);
+
+}
