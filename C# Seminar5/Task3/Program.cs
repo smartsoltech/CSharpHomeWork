1d4a267a1ed3f4a1bd6e6cbb727d8458321bca62
Andrew Choi <a.choi@smartsoltech.kr>
2022-09-15 23:08:42 +0900

    Seminra5. Task3, Fill array with float nummbers and count a difference between maximum ana minimak

---
+++ C# Seminar5/Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿int Prompt(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+float[] GenerateArray(int size)
+{
+    float[] numbers = new float[size];
+    System.Console.Write("[");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(1.00, 999.99);
+        System.Console.Write($"{numbers[i]} ");
+    }
+    System.Console.WriteLine("]");
+    System.Console.WriteLine();
+    return numbers;
+}
+int arrayLength = Prompt("Введите длину массива > ");
+float[] array = GenerateArray(arrayLength);
+for (int i=0; i<array.Length; i++)
+{
+    System.Console.WriteLine(array[i]);
+}
